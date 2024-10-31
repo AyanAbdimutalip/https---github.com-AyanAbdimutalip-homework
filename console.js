d9f225b2f2ab7e6deaf9ed018c3726f1de1f20a7
Ayan Abdimutalip <aianabdimutalip6@gmail.com>
2024-10-31 17:55:11 +0500

    ??

--- console.js
+++
@@ -1,9 +0,0 @@
-var name = "Ayan";
-var surname = "Abdimutalip";
-var birthday = "18.07.2009";
-var nation = "Kazakh";
-
-console.log("menin atym", name);
-console.log("menin tegim", surname);
-console.log("menin tuylgan kunym", birthday);
-console.log("menin otanym", nation);
---
+++ index.js
@@ -0,0 +1,67 @@
+// matematicalyk operatsia
+
+var a = 100;
+var b = 10;
+var c = 1;
+console.log( a / b / c ); //bolu
+
+var a = 100;
+var b = 10;
+var c = 1;
+console.log( a * b * c ); //kobeitu
+
+var a = 100;
+var b = 10;
+var c = 1;
+console.log( a - b - c ); //azaitu
+
+var a = 100;
+var b = 10;
+var c = 1;
+console.log( a + b + c );  //kosu
+
+var a = 22;
+var b = 4;
+console.log( a % b );  //kosu
+
+var a = 100;
+a++;
+a = a + 3;
+console.log(a)  //1 tal kosu
+
+var a = 100;
+a--;
+a = a - 3;
+console.log(a) //1 tal azaitu
+
+var a = 30;
+a += 30;
+console.log(a) //30 tal kosyp manin saktau
+
+var a = 30;
+a -= 15;
+console.log(a) //15 tal azaityp manin saktau
+
+var a = 30;
+a *= 30;
+console.log(a) //30 tal kobaitip manin saktau
+
+var a = 30;
+a /= 15;
+console.log(a) //15 tal bolip manin saktau
+
+var a = 30;
+var b = 3;
+a /= b;
+console.log(a)
+
+var a = 10;
+a %= 3;
+console.log(a);
+
+//operatordyn basymdu=ygy
+
+var num1 = 100;
+let total = ( a + )
+
+
